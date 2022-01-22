b79be081167b0ac83416add0e0baf627f375ce0d
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2022-01-22 18:31:06 -0500

    some configurations

---
+++ backend/run_lint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "running flake8 ..."
+flake8 --config=pyproject.toml --exclude=*/migrations/*
+
+echo ""
+
+echo "running black ..."
+black --check --diff --exclude=migrations --config=pyproject.toml backend
